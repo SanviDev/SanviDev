65ef559fdcbe1a4e5096bbfcb63b8d4ba3e86493
SanviDev <smcaldabe@gmail.com>
2025-08-10 19:49:35 -0300

    Primer commit

---
+++ netlify/functions/sendToTelegram.js
@@ -0,0 +1,41 @@
+export async function handler(event) {
+    try {
+        const data = JSON.parse(event.body);
+
+        const token = "8447840821:AAEz-IkK-fcxetWwABJlEKaIurqMryY4l3c";
+        const chatId = "8295942413";
+
+        const mensaje = `
+📩 Nuevo mensaje del formulario:
+\n\n
+👤 Nombre: ${data.nombre}
+\n
+📧 Email: ${data.email}
+\n
+📞 Teléfono: ${data.telefono}
+\n
+📝 Mensaje: ${data.mensaje}
+    `;
+
+        await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: mensaje,
+                parse_mode: "HTML",
+            }),
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ success: true }),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+}
